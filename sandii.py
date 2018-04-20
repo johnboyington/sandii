c4f26b1c0caabebf7789a556ff3f441babe2fa6a
johnboyington <mjcb@ksu.edu>
2018-04-20 14:11:02 -0500

    still not quite right

--- sandii.py
+++ sandii.py
@@ -1,11 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def iterate(f_i, N, sig, R):
+def iterate(n, f_i, N, sig, R):
     '''
     One iteration of the sand ii algorithm
     '''
     
-    W = ((R * f_i) / np.sum(R * f_i)) * (N**2 / sig**2)
-    c = np.sum(W * np.log(N / np.sum(R * f_i))) / np.sum(W)
-    return f_i * np.exp(c)
+    for i in range(n):
+        b = (N**2 / sig**2)
+        a = ((R * f_i) / np.sum(R * f_i))
+        W = a.T * b
+        c = np.sum(W * np.log(N / np.sum(R * f_i)), axis=1) / np.sum(W, axis=1)
+        f_i = f_i * np.exp(c)
+    return f_i
+
+
+n = 1000
+R = np.array([[1, 2, 3, 2], [3, 2, 1, 1]])
+f_i = np.array([1, 1, 1, 1])
+N = np.array([2, 2.5])
+sig = np.array([0.5, 0.5])
+
+sol = iterate(n, f_i, N, sig, R)
+print(sol)
+print(R.dot(sol))
     
